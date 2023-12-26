d2559b5b7d5130bc8cf69be67807efc6b3d05f5c
SURAJ <77872306+DrSuraj@users.noreply.github.com>
2023-12-26 15:22:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
-const arr = [1, 2, 3, 4, 1, 0, 2, 2];
+function divide(arr, n) {
+    // Initialize variables
+    let result = [];
+    let currentSubarray = [];
+    let currentSum = 0;
 
-const divide = (arr, n) => {
-  // Write your code here
-};
+    // Iterate over the input array
+    for (let i = 0; i < arr.length; i++) {
+        // If adding the current element doesn't exceed the maximum sum
+        if (currentSum + arr[i] <= n) {
+            currentSubarray.push(arr[i]);
+            currentSum += arr[i];
+        } else {
+            // Start a new subarray
+            result.push(currentSubarray);
+            currentSubarray = [arr[i]];
+            currentSum = arr[i];
+        }
+    }
 
-const n = prompt("Enter n: ");
-alert(JSON.stringify(divide(arr, n)));
+    // Add the last subarray to the result
+    result.push(currentSubarray);
+
+    return result;
+}
+
+// Examples
+console.log(divide([1, 2, 3, 4, 1, 0, 2, 2], 5)); // [[1, 2], [3], [4, 1, 0], [2, 2]]
+console.log(divide([4, 3, 2, 1], 4));           // [[4], [3], [2], [1]]
